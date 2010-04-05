f4bcc148c7c41d566e70ef582dfa70d7d362f9e6
Max Reitz <xanclic@googlemail.com>
2010-04-05 21:10:11 +0200

    CDI-13h
    * Läuft jetzt so einigermaßen

--- os-dep/cdi-13h/lib/advio.c
+++ os-dep/cdi-13h/lib/advio.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <cdi/bios.h>
+#include <cdi/io.h>
 #include <cdi/mem.h>
+#include <cdi/misc.h>
 
 #include "gbc.h"
 
 // Advanced I/O support for CDI systems (using mode 13h)
 
 static uint8_t *screen = NULL;
-static int scr_width;
+static int scr_width, last_key_update = -1, key_through = 0;
+
+uint32_t *old_vga_palette;
 
 void os_open_screen(int width, int height)
 {
@@ -23,6 +28,13 @@ void os_open_screen(int width, int height)
 
     screen = scr->vaddr;
 
+    if (cdi_ioports_alloc(0x3C8, 2))
+    {
+        cdi_mem_free(scr);
+        fprintf(stderr, "Could not allocate palette I/O ports!\n");
+        exit(1);
+    }
+
     if (cdi_bios_int10(&(struct cdi_bios_registers){ .ax = 0x13 }, NULL))
     {
         fprintf(stderr, "Could not switch to mode 13h.\n");
@@ -30,10 +42,38 @@ void os_open_screen(int width, int height)
     }
 
     scr_width = width;
+
+    memset(screen, 0, 64000);
+
+    old_vga_palette = malloc(1024);
+
+    for (int color = 0x00; color <= 0xFF; color++)
+    {
+        cdi_outb(0x3C8, color);
+        old_vga_palette[color] = cdi_inb(0x3C9);
+        old_vga_palette[color] |= cdi_inb(0x3C9) << 8;
+        old_vga_palette[color] |= cdi_inb(0x3C9) << 16;
+    }
+
+    cdi_outb(0x3C8, 0);
+    for (int color = 0x00; color <= 0xFF; color++)
+    {
+        cdi_outb(0x3C9, ((color & 0xE0) >> 5) *  9);
+        cdi_outb(0x3C9, ((color & 0x1C) >> 2) *  9);
+        cdi_outb(0x3C9,  (color & 0x03)       * 21);
+    }
 }
 
 static void leave_13h(void)
 {
+    cdi_outb(0x3C8, 0);
+    for (int color = 0x00; color <= 0xFF; color++)
+    {
+        cdi_outb(0x3C9,  old_vga_palette[color] & 0x0000FF       );
+        cdi_outb(0x3C9, (old_vga_palette[color] & 0x00FF00) >>  8);
+        cdi_outb(0x3C9, (old_vga_palette[color] & 0xFF0000) >> 16);
+    }
+
     cdi_bios_int10(&(struct cdi_bios_registers){ .ax = 0x1114, .bx = 0 }, NULL);
 }
 
@@ -45,13 +85,12 @@ void custom_exit(int num)
 
 void os_handle_events(void)
 {
-    int new_keystate = 0;
+    int new_keystate = keystates;
+    int input = 0;
 
-    clearerr(stdin);
-
-    while (!feof(stdin))
+    if (fread(&input, 1, 1, stdin))
     {
-        switch (getchar())
+        switch (input)
         {
             case 'a':
             case 'A':
@@ -68,15 +107,42 @@ void os_handle_events(void)
             case 'S':
                 new_keystate |= VK_SELECT;
                 break;
+            case 'q':
+            case 'Q':
+                custom_exit(0);
+                break;
             case ' ':
                 save_to_disk();
                 break;
-            // case LSHIFT: boost ^= 1; break; 
+            case 'f':
+            case 'F':
+                boost ^= 1;
+                break;
+            case 'i':
+            case 'I':
+                new_keystate |= VK_UP;
+                break;
+            case 'j':
+            case 'J':
+                new_keystate |= VK_LEFT;
+                break;
+            case 'k':
+            case 'K':
+                new_keystate |= VK_DOWN;
+                break;
+            case 'l':
+            case 'L':
+                new_keystate |= VK_RIGHT;
+                break;
         }
     }
 
     if (new_keystate != keystates)
     {
+        last_key_update = (unsigned)(io_regs->lyc - 1) & 0xFF;
+        if (last_key_update > 143)
+            last_key_update = 143;
+        key_through = 10;
         keystates = new_keystate;
         update_keyboard();
     }
@@ -86,12 +152,22 @@ void os_draw_line(int offx, int offy, int line)
 {
     int py = ((line + offy) & 0xFF) << 8;
 
+    if (line == last_key_update)
+    {
+        if (!--key_through)
+        {
+            last_key_update = -1;
+            keystates = 0;
+            update_keyboard();
+        }
+    }
+
     line *= 320;
 
     for (int x = 0; x < scr_width; x++)
     {
         int px = (x + offx) & 0xFF;
         uint32_t col = ((uint32_t *)vidmem)[py + px];
-        screen[line + x] = col & 0xFF;
+        screen[line + x] = (((col & 0xFF0000) >> 16) & 0xE0) | (((col & 0xFF00) >> 11) & 0x1C) | ((col & 0xFF) >> 6);
     }
 }
